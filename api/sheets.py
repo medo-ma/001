9eac61b8ef85ec194323c8ce7891ffdea216f061
medo-ma <mohamme123git@gmail.com>
2024-09-04 21:14:02 +0300

    Update sheets.py

--- api/sheets.py
+++ api/sheets.py
@@ -12,7 +12,7 @@ credentials = Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build('sheets', 'v4', credentials=credentials)
 
-SPREADSHEET_ID = '1435980372'  # Replace with your Google Sheets ID
+SPREADSHEET_ID = '14B1_20Ix3CjgrUxijbYpIhpFQVa1ai3'  # Replace with your Google Sheets ID
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
