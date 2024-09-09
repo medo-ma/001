738367a055c2ab9f35accc9045ad919ec83ebba2
medo-ma <mohamme123git@gmail.com>
2024-09-10 02:48:33 +0300

    Update sheets.py

--- api/sheets.py
+++ api/sheets.py
@@ -160,6 +160,7 @@ class handler(BaseHTTPRequestHandler):
 #send the response
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(json.dumps({'status': 'success', 'mo ':f'{values}'}).encode())
 
